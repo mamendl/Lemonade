46476248e6a05d4d12819c9f751c1ca90ee9fdc3
mamendl <mamendl@unirioja.es>
2024-02-19 20:05:48 +0100

    terminao

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.lemonade
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,9 +13,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,6 +29,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lemonade.ui.theme.LemonadeTheme
@@ -45,57 +51,122 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LemonApp() {
     // Current step the app is displaying (remember allows the state to be retained
     // across recompositions).
     var currentStep by remember { mutableStateOf(1) }
 
-    // A surface container using the 'background' color from the theme
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
+    var squeezCount by remember { mutableStateOf(0) }
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Lemonade",
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            )
+        }
     ) {
-        when (currentStep) {
-            1 -> {
-                Column (
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier.fillMaxSize()
-                ){
-                    Text(text = stringResource(R.string.tap_tree))
-                    Spacer(modifier = Modifier.height(32.dp))
-                    Image(
-                        painter = painterResource(R.drawable.lemon_tree),
-                        contentDescription = stringResource(R.string.lemon_tree_desc),
-                        modifier = Modifier
-                            .wrapContentSize()
-                            .clickable {
-                                currentStep = 2
-                            }
-                    )
+
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            when (currentStep) {
+                1 -> {
+                    Column (
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ){
+                        Text(text = stringResource(R.string.tap_tree))
+                        Spacer(modifier = Modifier.height(32.dp))
+                        Image(
+                            painter = painterResource(R.drawable.lemon_tree),
+                            contentDescription = stringResource(R.string.lemon_tree_desc),
+                            modifier = Modifier
+                                .wrapContentSize()
+                                .clickable {
+                                    currentStep = 2
+                                    squeezCount = (2..4).random()
+                                }
+                        )
+                    }
+                }
+                2 -> {
+                    Column (
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ){
+                        Text(text = stringResource(R.string.tap_lemon))
+                        Spacer(modifier = Modifier.height(32
+                            .dp))
+                        Image(
+                            painter = painterResource(R.drawable.lemon_squeeze),
+                            contentDescription = stringResource(R.string.lemon_desc),
+                            modifier = Modifier.wrapContentSize()
+                                .clickable {
+                                    squeezCount--
+                                    if(squeezCount==0){
+                                        currentStep = 3
+                                    }
+                                }
+                        )
+                    }
+                }
+                3->{
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ){
+                        Text(text = stringResource(R.string.tap_lemonade))
+                        Spacer(modifier = Modifier.height(32.dp))
+                        Image(
+                            painter = painterResource(R.drawable.lemon_drink),
+                            contentDescription = stringResource(R.string.lemonade_glass_desc),
+                            modifier = Modifier
+                                .wrapContentSize()
+                                .clickable {
+                                    currentStep = 4
+                                }
+                        )
+                    }
+                }
+                4-> {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Text(text = stringResource(R.string.tap_glass))
+                        Spacer(modifier = Modifier.height(32.dp))
+                        Image(
+                            painter = painterResource(R.drawable.lemon_restart),
+                            contentDescription = stringResource(R.string.empty_glass_desc),
+                            modifier = Modifier
+                                .wrapContentSize()
+                                .clickable {
+                                    currentStep = 1
+                                }
+                        )
+                    }
                 }
             }
-            2 -> {
-                Column (
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier.fillMaxSize()
-                ){
-                    Text(text = stringResource(R.string.tap_lemon))
-                    Spacer(modifier = Modifier.height(32
-                        .dp))
-                    Image(
-                        painter = painterResource(R.drawable.lemon_squeeze),
-                        contentDescription = stringResource(R.string.lemon_desc),
-                        modifier = Modifier.wrapContentSize()
-                            .clickable {
-                                currentStep = 3
-                            }
-                    )
-                }
-            }
-            3->{}
         }
     }
 }
@@ -103,7 +174,7 @@ fun LemonApp() {
 @Composable
 fun WelcomeScreen(name: String, onStartClicked: () -> Unit) {
     Column {
-        Text(text = "Welcome $name!")
+        Text(text = "Lemonade!")
         Button(
             onClick = onStartClicked
         ) {
@@ -112,7 +183,7 @@ fun WelcomeScreen(name: String, onStartClicked: () -> Unit) {
     }
 }
 
-@Preview(showBackground = true)
+
 @Composable
 fun DefaultPreview() {
     LemonadeTheme {
